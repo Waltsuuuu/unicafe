e2e965807db90d38f2db405c45d8742ef0734736
Waltsuuu <waltteriheino@gmail.com>
2024-08-23 12:56:58 +0300

    Finished unicafe step 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,20 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allFeedback, setAllFeedback] = useState(0)
+  const allFeedback = good + neutral + bad
 
-  const handleGood = () => {
-    setAllFeedback(allFeedback + 1)
-    const updatedGood = good + 1
-    setGood(updatedGood)
-  }
-  const handleNeutral = () => {
-    setAllFeedback(allFeedback + 1)
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-  }
-  const handleBad = () => {
-    setAllFeedback(allFeedback + 1)
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-  }
+  // Laske keskiarvo: (good * 1 + neutral * 0 + bad * -1) / allFeedback
+  // Keskiarvo lasketaan kaavalla (good - bad) / allFeedback, mikä tarkoittaa, 
+  // että hyvä palaute on arvoltaan 1, neutraali 0 ja huono -1.
+  const average = allFeedback > 0 ? (good - bad) / allFeedback : 0
+
+  // Prosenttiosuus lasketaan kaavalla (good / allFeedback) * 100.
+  const positivePercentage = allFeedback > 0 ? (good / allFeedback) * 100 : 0 
+
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad +1)
 
   return (
     <>
@@ -35,7 +32,9 @@ const App = () => {
       <Statistics label={'good'} stats={good}/>
       <Statistics label={'neutral'} stats={neutral}/>
       <Statistics label={'bad'} stats={bad}/>
-      <Statistics label={'all'} stats={allFeedback}/>      
+      <Statistics label={'all'} stats={allFeedback}/>    
+      <Statistics label={'avg'} stats={average} />
+      <Statistics label={'positive'} stats={positivePercentage}/>
     </>
   )
 }
